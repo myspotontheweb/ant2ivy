9b00f8e41b1a28b0588af327792b9836a9f14a3d
Mark O'Connor <mark@myspotontheweb.com>
2011-09-24 16:44:25 +0100

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,29 @@
+
+Introduction
+============
+The purpose of this groovy script is to "kick-start" a Java project's use of
+the ivy plugin for ANT.
+
+Parameters
+==========
+
+    $ groovy ant2ivy --help
+    usage: ant2ivy
+     -a,--artifactid <arg>   Module artifactid
+     -g,--groupid <arg>      Module groupid
+     -h,--help               Show usage information
+     -s,--sourcedir <arg>    Source directory containing jars
+     -t,--targetdir <arg>    Target directory where write ivy build files
+
+Example
+-------
+The following example scans tomcat's libraries and generates the ivy files to download them from Maven Central
+
+    $ groovy ant2ivy -g com.example -a demo -s /opt/apache-tomcat-7.0.21/lib -t build
+    13 [main] INFO Ant2Ivy - Searching: http://repository.sonatype.org ...
+    3910 [main] WARN Ant2Ivy - Not Found: ecj-3.7.jar
+    8025 [main] INFO Ant2Ivy - Generating ant file: /home/mark/Files/ant2ivy/build/build.xml ...
+    8036 [main] INFO Ant2Ivy - Generating ivy file: /home/mark/Files/ant2ivy/build/ivy.xml ...
+    8051 [main] INFO Ant2Ivy - Generating ivy settings file: /home/mark/Files/ant2ivy/build/ivysettings.xml ...
+         [copy] Copying 1 file to /home/mark/Files/ant2ivy/build/jars
+
---
+++ ant2ivy.groovy
@@ -0,0 +1,188 @@
+//
+// Dependencies
+// ============
+
+import groovy.xml.MarkupBuilder
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Grapes([
+    @Grab(group='org.slf4j', module='slf4j-simple', version='1.6.2') 
+])
+
+//
+// Classes
+// =======
+
+class Ant2Ivy {
+
+    Logger log = LoggerFactory.getLogger(this.class.name);
+    String groupId
+    String artifactId
+    String repoUrl
+
+    Ant2Ivy(groupId, artifactId) {
+        this(groupId, artifactId, "http://repository.sonatype.org")
+    }
+
+    Ant2Ivy(groupId, artifactId, repoUrl) {
+        this.groupId = groupId
+        this.artifactId = artifactId
+        this.repoUrl = repoUrl
+
+        log.debug "groupId: {}, artifactId: {}", groupId, artifactId
+    }
+
+    //
+    // Given a directory, find all jar and search Nexus
+    // based on the file's checksum
+    //
+    // Return a data structure containing the GAV coordinates of each jar
+    //
+    def search(File inputDir) {
+        def results = [:]
+        results["found"] = []
+        results["missing"] = []
+
+        log.info "Searching: {} ...", repoUrl
+
+        def ant = new AntBuilder()
+        ant.fileset(id:"jars", dir:inputDir.absolutePath, includes:"**/*.jar")
+
+        ant.project.references.jars.each {
+            def jar = new File(inputDir, it.name)
+
+            // Checksum URL
+            ant.checksum(file:jar.absolutePath, algorithm:"SHA1", property:jar.name)
+
+            def searchUrl = "${repoUrl}/service/local/data_index?sha1=${ant.project.properties[jar.name]}"
+            log.debug "SearchUrl: {}, File: {}", searchUrl, jar.name
+
+            // Search for the first result
+            def searchResults = new XmlParser().parseText(searchUrl.toURL().text)
+            def artifact = searchResults.data.artifact[0]
+
+            if (artifact) {
+                log.debug "Found: {}", jar.name
+                results["found"].add([file:jar.name, groupId:artifact.groupId.text(), artifactId:artifact.artifactId.text(), version:artifact.version.text()])
+            }
+            else {
+                log.warn "Not Found: {}", jar.name
+                results["missing"].add([file:jar.name, fileObj:jar])
+            }
+        }
+
+        return results
+    }
+
+    //
+    // Given an input direcory, search for the GAV coordinates 
+    // and use this information to write two XML files:
+    //
+    // ivy.xml          Contains the ivy dependency declarations
+    // ivysettings.xml  Resolver configuration
+    //
+    def generate(File inputDir, File outputDir) {
+        outputDir.mkdir()
+
+        def antFile = new File(outputDir, "build.xml")
+        def ivyFile = new File(outputDir, "ivy.xml")
+        def ivySettingsFile = new File(outputDir, "ivysettings.xml")
+        def localRepo = new File(outputDir, "jars")
+        def results = search(inputDir)
+
+        //
+        // Generate the ant build file
+        //
+        log.info "Generating ant file: {} ...", antFile.absolutePath
+        def antContent = new MarkupBuilder(antFile.newPrintWriter())
+
+        antContent.project(name: "Sample ivy builde", default:"resolve", "xmlns:ivy":"antlib:org.apache.ivy.ant" ) {
+            target(name:"resolve") {
+                "ivy:resolve"()
+            }
+            target(name:"clean") {
+                "ivy:cleancache"()
+            }
+        }
+
+        // 
+        // Generate the ivy file
+        //
+        log.info "Generating ivy file: {} ...", ivyFile.absolutePath
+        def ivyConfig = new MarkupBuilder(ivyFile.newPrintWriter())
+
+        ivyConfig."ivy-module"(version:"2.0") {
+            info(organisation:this.groupId, module:this.artifactId) 
+            configurations(defaultconfmapping:"default")
+            dependencies() {
+                results.found.each {
+                    dependency(org:it.groupId, name:it.artifactId, rev:it.version, conf:"default->master")
+                }
+                results.missing.each {
+                    dependency(org:"NA", name:it.file, rev:"NA")
+                }
+            }
+        }
+
+        // 
+        // Generate the ivy settings file
+        //
+        log.info "Generating ivy settings file: {} ...", ivySettingsFile.absolutePath
+        def ivySettings = new MarkupBuilder(ivySettingsFile.newPrintWriter())
+        def ant = new AntBuilder()
+
+        ivySettings.ivysettings() {
+            settings(defaultResolver:"maven-repos") 
+            resolvers() {
+                chain(name:"maven-repos") {
+                    // TODO: Make this list of Maven repos configurable
+                    ibiblio(name:"central", m2compatible:"true")
+                    ibiblio(name:"spring-external", m2compatible:"true", root:"http://repository.springsource.com/maven/bundles/external")
+                }
+                if (results.missing.size() > 0) {
+                    filesystem(name:"local") {
+                        artifact(pattern:"${localRepo.absolutePath}/[artifact]")
+                    }
+                }
+            }
+            if (results.missing.size() > 0) {
+                modules() {
+                    results.missing.each {
+                        module(organisation:"NA", name:it.file, resolver:"local")
+                        ant.copy(file:it.fileObj.absolutePath, tofile:"${localRepo.absolutePath}/${it.file}")
+                    }
+                }
+            }
+        }
+    }
+}
+
+// 
+// Main program
+// ============
+def cli = new CliBuilder(usage: 'ant2ivy')
+cli.with {
+    h longOpt: 'help', 'Show usage information'
+    g longOpt: 'groupid',    args: 1, 'Module groupid', required: true
+    a longOpt: 'artifactid', args: 1, 'Module artifactid', required: true
+    s longOpt: 'sourcedir',  args: 1, 'Source directory containing jars', required: true
+    t longOpt: 'targetdir',  args: 1, 'Target directory where write ivy build files', required: true
+}
+                                                                
+def options = cli.parse(args)
+if (!options) {
+    return
+}
+
+if (options.help) {
+    cli.usage()
+}
+
+// 
+// Generate ivy configuration
+//
+def ant2ivy = new Ant2Ivy(options.groupid, options.artifactid)
+ant2ivy.generate(new File(options.sourcedir), new File(options.targetdir))
+
